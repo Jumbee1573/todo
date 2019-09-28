1c278b7d8200e03029add80f5048ffe9994d9dd4
Jumbee1573 <stasik1999box@gmail.com>
2019-09-28 19:19:20 +0300

    Updates

---
+++ precache-manifest.d4b9ea2614071c65b5e3135cc446dc90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227b20264c57c31b1793028a78cb95a8",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "c3860c951822e774d52f",
+    "url": "/todo/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "282cf8209313f9b420b0",
+    "url": "/todo/static/css/main.3b497325.chunk.css"
+  },
+  {
+    "revision": "c3860c951822e774d52f",
+    "url": "/todo/static/js/2.0bb79a1c.chunk.js"
+  },
+  {
+    "revision": "282cf8209313f9b420b0",
+    "url": "/todo/static/js/main.061f0f46.chunk.js"
+  },
+  {
+    "revision": "5be628b1d2ba8eaadf9c",
+    "url": "/todo/static/js/runtime~main.1607041f.js"
+  }
+]);
